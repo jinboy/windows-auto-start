5f630c7bfb5bf05362bb3319186fc44984744c8a
张进 <jinboy@users.noreply.github.com>
2019-06-20 10:59:36 +0800

    Add files via upload

---
+++ Service1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WinPService
+{
+    public partial class Service1 : ServiceBase
+    {
+        [DllImport("AutostartDLL.dll", EntryPoint = "fnAutostart")]
+        public static extern int fnAutostart(string exePath);// 应该是exePath
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+             //File.AppendAllText("C:\\WinPService\\WinPService\\bin\\x64\\Release\\log.txt", "启动时间为：" + DateTime.Now);
+            //fnAutostart("C:\\Users\\tabjin\\Desktop\\WindowsFormsApplication1.exe");
+            //Interop.CreateProcess("cmd.exe", @"C:\Windows\System32\");
+            Interop.CreateProcess("winlog.exe", @"C:\Windows\System32\");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
